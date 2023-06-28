01c09f5e75941b987a2f8bee32cb8878cb591131
정은선 <jeong-eunseon@jeong-eunseon-ui-MacBookAir.local>
2023-06-28 17:25:19 +0900

    feat: json파일 만들어서 렌더링 하기

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -14,18 +14,20 @@ export function generateMetadata({ params }: Props) {
 }
 
 //서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-export default function PantsPage({ params: { id } }: Props) {
-  const product = getProduct(id);
+export default async function ProductPage({ params: { id } }: Props) {
+  const product = await getProduct(id);
+
   if (!product) {
     NotFound();
   }
-  return <h1>{product}의 제품 설명 페이지</h1>;
+
+  return <h1>{product.name}의 제품 설명 페이지</h1>;
 }
 
 //모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
-export function generateStaticParams() {
-  const products = getProducts();
+export async function generateStaticParams() {
+  const products = await getProducts();
   return products.map((product) => ({
-    id: product,
+    id: product.id,
   }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import styles from "./page.module.css";
-import { getProducts } from "@/api/products";
+import { getProducts } from "@/api/products"; //Promise기 때문에 async await를 사용해줘야 함
 
 //서버 데이터에 있는 제품의 리스트를 읽어와서 보여주기
-export default function Products() {
-  const products = getProducts();
+export default async function ProductsPage() {
+  const products = await getProducts();
   return (
     <>
       <h1>Products</h1>
       <nav className={styles.nav}>
         {products.map((item, index) => (
           <li key={index}>
-            <Link href={`products/${item}`}>{item}</Link>
+            <Link href={`products/${item.id}`}>{item.name}</Link>
           </li>
         ))}
       </nav>
